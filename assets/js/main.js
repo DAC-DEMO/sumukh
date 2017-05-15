dd2d757b8cb408fdf0d647be42586310fa01a7f7
santosh <santosh.ece06@gmail.com>
2017-05-15 11:30:36 +0530

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,7 @@ console.log("Hello World");
 function createPost(elemObj) {
     var post = $("#postId").val();
 
+
     var inputData = {
         "POST": post,
         "POST_TYPE": "TEXT",
@@ -12,11 +13,39 @@ function createPost(elemObj) {
     var jqxhr = $.post(url, inputData);
     jqxhr.done(function(data) {
         console.log(data);
+        readAllPost(); //refresh new contents
+        $("#postId").val("");
     });
 
     jqxhr.fail(function(err) {
         console.log(err);
     });
-
-
 }
+
+function readAllPost() {
+    var url = "http://localhost:3010/post/";
+    var jqxhr = $.get(url);
+    jqxhr.done(function(data) {
+        console.log(data);
+        displayInitialPost(data);
+    });
+
+    jqxhr.fail(function(err) {
+        console.log(err);
+    });
+}
+
+function displayInitialPost(data) {
+    if (data) {
+        for (var i = 0; i < data.length; i++) {
+            var newTemplate = $("#templateId").clone(true).css("display", "block").removeAttr("id");
+            newTemplate.children().first().children().first().html(data[i].POST);
+
+
+
+            $("#parentId").prepend(newTemplate);
+        }
+    }
+}
+
+readAllPost();
